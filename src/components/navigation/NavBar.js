78f712878e9b6c3095a65eae350e457517e5e14e
Jarrod Kallis <jarrod.kallis@gmail.com>
2019-03-31 20:36:18 +0200

    Initial Commit

---
+++ src/components/navigation/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './NavBar.css';
+
+const NavBar = () => (
+  <nav className="navigation-toolbar">
+    <ul>
+      <li>
+        <a href="#">Products</a>
+      </li>
+      <li>
+        <a href="#">Cart</a>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default NavBar;
